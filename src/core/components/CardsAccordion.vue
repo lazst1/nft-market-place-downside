f68be6789d30abcfb6ca4529facd15d48572e3c0
rockdev <19998916+rockdev101@users.noreply.github.com>
2022-01-05 21:18:10 -0800

    feat: home page theming;

---
+++ src/core/components/CardsAccordion.vue
@@ -0,0 +1,32 @@
+<template>
+    <accordion :border="false">
+        <template v-slot:caption>
+            <div class="flex items-center w-full">
+                <div class="flex-1 text-sm font-press py-4 mr-24">
+                    Trending Collections
+                </div>
+                <nftmx-select class="font-ibm font-thin w-full max-w-lg text-sm" :data="items" small>
+                </nftmx-select>
+            </div>
+        </template>
+        <div class="px-6 pt-2">
+            <div class="grid gap-4 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-5">
+                <nftmx-sale-card :auction="true"></nftmx-sale-card>
+                <nftmx-sale-card></nftmx-sale-card>
+                <nftmx-sale-card :syndication="false"></nftmx-sale-card>
+                <nftmx-sale-card v-for="index in 2" :key="index" :bought="true"></nftmx-sale-card>
+            </div>
+        </div>
+    </accordion>
+</template>
+
+<script>
+import Accordion from '@/core/container/Accordion.vue';
+
+
+export default {
+    setup() {
+        
+    },
+}
+</script>
