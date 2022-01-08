e95279a9a8d6f69c375305ade9a424374037b3cd
rockdev <19998916+rockdev101@users.noreply.github.com>
2022-01-08 23:22:22 +0800

    fix: Sidebar importing

--- src/core/components/Sidebar.vue
+++ src/core/components/Sidebar.vue
@@ -1,6 +1,11 @@
+<script setup>
+import Accordion from '@/core/container/Accordion.vue';
+import NftmxDivider from './NftmxDivider.vue';
+</script>
+
 <template>
-    <div class="h-screen overflow-auto fixed z-20 w-72 top-0 right-0 bg-black text-white pt-20 pl-8 pr-2 font-ibm text-xs leading-7.5">
-        <div class="font-ibm-semi-bold text-primary-900">Create NFT</div>
+    <div class="absolute z-20 w-72 top-0 right-0 bg-black text-white pt-20 pl-8 pr-2 font-ibm text-xs leading-7.5">
+        <div class="font-ibm-semi-bold text-primary-900"><router-link to="">Create NFT</router-link></div>
         <div class="font-ibm-semi-bold text-primary-900">Stake / Sell NFT</div>
         <div class="font-ibm-semi-bold text-primary-900">Stake / Sell Land</div>
         <div class="font-ibm-semi-bold text-secondary-900">Free license</div>
