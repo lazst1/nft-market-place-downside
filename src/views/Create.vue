c90986404b66891800771904e9adece405452a2b
rockdev <19998916+rockdev101@users.noreply.github.com>
2022-01-13 10:02:31 +0800

    feat: NftmxSellGrid

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -3,6 +3,7 @@ import BodyContainer from '@/core/container/BodyContainer.vue';
 import NftmxFooter from '@/core/container/NftmxFooter.vue';
 import NftmxSelect from '@/core/components/NftmxSelect.vue';
 import NftmxSelectNetwork from '@/core/components/NftmxSelectNetwork.vue';
+import NftmxSellGrid from '@/core/components/NftmxSellGrid.vue';
 const people = [
     {
         name: 'Wade Cooper',
@@ -17,18 +18,40 @@ const people = [
 
 <template>
     <body-container>
-        <div class="grid grid-cols-3 text-white">
-            <div class="col-span-1" />
-            <div class="col-span-2 font-press text-3xl">
-                Create an NFT
-            </div>
-            <div class="col-span-1 font-ibm-bold text-lg">
-                Blockchain
-            </div>
-            <div class="col-span-2 font-ibm text-sm text-tertiary-400">
-                <nftmx-select-network />
-            </div>
-        </div>
+        <nftmx-sell-grid class="my-2.5">
+            <template v-slot:value>
+                <div class="col-span-2 font-press text-3xl">
+                    Create an NFT
+                </div>
+            </template>
+        </nftmx-sell-grid>
+        <nftmx-sell-grid class="mt-13.5">
+            <template v-slot:item>
+                <div class="font-ibm-bold text-lg pt-3.5">
+                    Blockchain
+                </div>
+            </template>
+            <template v-slot:value>
+                <div class="font-ibm text-sm text-tertiary-400">
+                    <nftmx-select-network />
+                </div>
+            </template>
+        </nftmx-sell-grid>
+        <nftmx-sell-grid class="mt-10">
+            <template v-slot:item>
+                <div class="font-ibm-bold text-lg pt-3">
+                    Image, Video, Audio, or 3D Model
+                </div>
+                <div class="font-ibm-medium text-sm text-tertiary-500 pt-1">
+                    File types supported: JPG, PNG, GIF, SVG, MP4, WEBM, MP3, WAV, OGG, GLB, GLTF. Max size: 40 MB
+                </div>
+            </template>
+            <template v-slot:value>
+                <div class="font-ibm text-sm text-tertiary-400">
+                    <nftmx-select-network />
+                </div>
+            </template>
+        </nftmx-sell-grid>
     </body-container>
     <nftmx-footer />
 </template>
