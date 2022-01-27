c4a3e78ee31cd1485d610a75fe7d906e6657f1dd
rockdev <19998916+rockdev101@users.noreply.github.com>
2022-01-28 04:21:39 +0800

    fix: moralis start

--- src/plugins/moralis.js
+++ src/plugins/moralis.js
@@ -1,10 +1,9 @@
 import Moralis from "moralis";
 
 console.log(import.meta.env)
+const serverUrl = import.meta.env.VITE_APP_MORALIS_SERVER_URL;
+const appId = import.meta.env.VITE_APP_MORALIS_APP_ID;
 
-Moralis.start({
-  serverUrl: import.meta.env.VITE_APP_MORALIS_SERVER_URL,
-  appId: import.meta.env.VITE_APP_MORALIS_APP_ID,
-});
+Moralis.start({serverUrl, appId});
 
 export default Moralis;
