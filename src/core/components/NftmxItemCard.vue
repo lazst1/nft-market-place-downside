f3c956c5fe6735cc935a4da72a97587575ce1ab2
rockdev <19998916+rockdev101@users.noreply.github.com>
2022-01-28 21:04:58 +0800

    fix: icon modal

--- src/core/components/NftmxItemCard.vue
+++ src/core/components/NftmxItemCard.vue
@@ -106,32 +106,49 @@ function onClickOutside() {
 </script>
 
 <template>
-    <nftmx-card-container :forMore="forMore" :image="metadata?metadata.image:''" :assetContractAddress="item.token_address" :tokenId="item.token_id">
+    <nftmx-card-container
+        :forMore="forMore"
+        :image="metadata ? metadata.image : ''"
+        :assetContractAddress="item.token_address"
+        :tokenId="item.token_id"
+    >
         <div class="h-">
-            <div :class="[forMore?'h-17':'h-24', 'mb-0.5']">
-                <div class="text-tertiary-400 text-xxs">
-                    {{item.name?item.name:''}}
-                </div>
-                <div class="text-white font-ibm-medium text-sm leading-5.25 my-0.75">
-                    {{metadata.name?metadata.name:''}}
-                </div>
+            <div :class="[forMore ? 'h-17' : 'h-24', 'mb-0.5']">
+                <div class="text-tertiary-400 text-xxs">{{ item.name ? item.name : '' }}</div>
+                <div
+                    class="text-white font-ibm-medium text-sm leading-5.25 my-0.75"
+                >{{ metadata.name ? metadata.name : '' }}</div>
                 <div v-if="!forMore" class="text-tertiary-400 font-ibm-light text-xxs leading-5.25">
-                    <text-compression :text="metadata.description?metadata.description:''" :length="40" />
+                    <text-compression
+                        :text="metadata.description ? metadata.description : ''"
+                        :length="40"
+                    />
                 </div>
             </div>
             <div class="flex items-center font-ibm-semi-bold mt-1">
                 <div class="text-sm flex flex-1 items-center">
-                    <div @click.prevent v-show="option" class="absolute -bottom-7 -ml-5 px-5.5 py-3.25 bg-black text-white hover:text-primary-900 leading-9.5 font-ibm-light text-xs">
+                    <div
+                        @click.prevent
+                        v-show="option"
+                        class="absolute -bottom-2.5 -ml-5 px-5.5 py-3.25 bg-black text-white hover:text-primary-900 leading-9.5 font-ibm-light text-xs"
+                    >
                         <div class="text-white hover:text-primary-700">Copy link</div>
                         <div class="text-white hover:text-primary-700">Transfer</div>
                         <div class="text-white hover:text-primary-700">Make profile picture</div>
                         <div class="text-white hover:text-primary-700">Hide</div>
                         <div class="text-white hover:text-primary-700">Unbundle</div>
                     </div>
-                    <div v-click-outside="onClickOutside" @click="option = !option" @click.prevent class="pr-3 mr-px text-white hover:text-primary-900 text-lg">
+                    <div
+                        v-click-outside="onClickOutside"
+                        @click="option = !option"
+                        @click.prevent
+                        class="pr-3 mr-px text-white hover:text-primary-900 text-lg"
+                    >
                         <font-awesome-icon :icon="['fas', 'ellipsis-v']" />
                     </div>
-                    <checkbox-cell @click.stop v-if="!forMore" border="border-tertiary-500" small><span class="text-xs relative text-tertiary-500 -ml-2">Sell</span></checkbox-cell>
+                    <checkbox-cell @click.stop v-if="!forMore" border="border-tertiary-500" small>
+                        <span class="text-xs relative text-tertiary-500 -ml-2">Sell</span>
+                    </checkbox-cell>
                 </div>
                 <div class="text-xs text-tertiary-500 flex items-end font-ibm-medium">
                     <span style="line-height: 0.75" class="pr-2">124</span>
