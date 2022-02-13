2d63872fd429b426b52a3b09fa5bf5b0bd2ce105
rockdev <19998916+rockdev101@users.noreply.github.com>
2022-02-14 00:19:26 +0800

    feat: landing section

--- src/views/landing/LandingDescription.vue
+++ src/views/landing/LandingDescription.vue
@@ -1,26 +1,27 @@
 <script setup>
 import NftmxTypography from '@/core/components/NftmxTypography.vue';
+import LandingSection from './containers/LandingSection.vue';
 
 </script>
 
 <template>
-    <div class="bg-black text-white pt-24 pb-2.5">
-        <div class="font-press text-center pt-0.75">
+    <landing-section black>
+        <div class="font-press text-center xl:mt-0.75">
             <nftmx-typography h1>Downside Protection</nftmx-typography>
         </div>
-        <div class="grid grid-cols-2 px-4 lg:px-22 gap-10 text-center pt-12 pb-14">
-            <div class="py-0.5 col-span-2 sm:col-span-1">
-                <div class="text-2xl font-ibm-bold">For Buyer</div>
+        <div class="grid sm:grid-cols-2 px-4 lg:px-22 sm:gap-10 text-center pt-6 lg:pt-12 pb-14">
+            <div class="py-0.5">
+                <div class="text-xl lg:text-2xl font-ibm-bold">For Buyer</div>
                 <div
-                    class="text-tertiary-500 py-7 mt-px"
+                    class="text-tertiary-500 py-5 lg:py-7 mt-px"
                 >If you are a buyer, think of NFT.mx as a new strategic staking program with upside from selling the NFT, while also providing the option to cancel your investment and get a 100% refund with your original tokens.</div>
             </div>
-            <div class="col-span-2 sm:col-span-1">
-                <div class="text-2xl font-ibm-bold">For Seller</div>
+            <div class="">
+                <div class="text-xl lg:text-2xl font-ibm-bold">For Seller</div>
                 <div
                     class="text-tertiary-500 py-7"
                 >If you are a seller, NFT.mx allows you to increase your success rate by offering buyers downside protection all while gaining profits from the APY in leveraging other people's money.</div>
             </div>
         </div>
-    </div>
+    </landing-section>
 </template>
---
+++ src/views/landing/containers/LandingSection.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+    black: Boolean
+})
+
+</script>
+
+<template>
+    <div :class="[black ? 'bg-black' : '', 'text-white pt-14 lg:pt-24']">
+        <slot />
+    </div>
+</template>
