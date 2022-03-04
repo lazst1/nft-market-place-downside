1613c7c05280d74b6c637c07eb343dc2ff226de9
rockdev <19998916+rockdev101@users.noreply.github.com>
2022-03-04 13:16:17 +0800

    chore: update 'approve to list' card design

--- src/core/container/NftmxCardContainer.vue
+++ src/core/container/NftmxCardContainer.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Ribbon from '@/core/components/Ribbon.vue';
+import NftmxButton from '../components/NftmxButton.vue';
 import { computed, ref } from 'vue';
 
 const props = defineProps({
@@ -22,24 +23,30 @@ const tokenImage = ref(props.image || 'images/img10.png')
 <template>
     <div class="w-full relative">
         <div
-            class="transition hover:shadow-[0_0px_12px_0px_rgb(0_0_0_/_0.1),_0_0px_0px_0px_rgb(0_0_0_/_0.1);] hover:shadow-primary-700"
+            :class="{'transition hover:shadow-[0_0px_12px_0px_rgb(0_0_0_/_0.1),_0_0px_0px_0px_rgb(0_0_0_/_0.1);] hover:shadow-primary-700': approved}"
         >
-            <div class="bg-black">
+            <div class="bg-black relative">
                 <div
                     class="relative w-full pt-65per overflow-hidden p-6"
                     :style="{ background: 'url(' + tokenImage + ')', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center' }"
                 />
+                <div
+                    v-if="!approved"
+                    class="absolute top-0 left-0 w-full h-full bg-black bg-opacity-80 flex justify-center items-center"
+                >
+                    <nftmx-button
+                        label="Approve To List"
+                        color="primary"
+                        outline
+                        class="font-press z-10 text-primary-400"
+                        @click="emit('approve')"
+                    />
+                </div>
             </div>
             <div class="p-5">
                 <slot />
             </div>
         </div>
-        <div
-            v-if="!approved"
-            class="absolute top-0 transition w-full h-full bg-black bg-opacity-80 hover:bg-primary-900 hover:opacity-80 text-primary-400 hover:text-white hover:cursor-pointer flex justify-center items-center"
-            @click="emit('approve')"
-        >
-            <div class="w-fit font-press text-xs">Approve To List</div>
-        </div>
+        <div v-if="!approved" class="absolute top-0 transition w-full h-full" />
     </div>
 </template>
